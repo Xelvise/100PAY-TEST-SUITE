99a2ef21b7b11332f5e03d957414d480e19c3d91
Xelvis <elvisgideonuzuegbu@gmail.com>
2024-08-10 13:48:27 +0100

    updated workflow config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: process.env.URL,
     ignoreHTTPSErrors: true,
-    headless: true,
+    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     permissions: ["clipboard-read"],
